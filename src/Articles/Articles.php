099a7441182cb4b1a348527c96a82e811dfdeaa6
daniel21703 <daniel217032001@gmail.com>
2024-02-10 12:38:26 +0300

    Update Articles\Articles

--- src/Articles/Articles.php
+++ src/Articles/Articles.php
@@ -3,12 +3,24 @@
 namespace DDaniel\Blog\Articles;
 
 class Articles {
-	public readonly array $names;
+	/**
+	 * @var Article[]
+	 */
+	protected array $articles;
 
 	public function __construct() {
-		$this->names = array_map(
-			fn( $path ) => basename( $path, '.md' ),
+		$this->articles = array_map(
+			fn( $path ) => new Article( basename( $path, '.md' ) ),
 			glob( app()->path . 'content/*.md' )
 		);
+	}
+
+	public function get_content_html(): string {
+		$articles = $this->articles;
+
+		ob_start();
+		require app()->path . 'templates/articles.php';
+
+		return ob_get_clean();
 	}
 }
