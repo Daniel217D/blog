a184c5efdf699cd344362cc7ab2e4c39f3e3029d
daniel21703 <daniel217032001@gmail.com>
2024-02-18 00:24:39 +0300

    Update header

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,12 @@
 <header class="p-sm-3 pt-3 mb-2 bg-dark text-white border-bottom">
 	<div class="container">
-		<div class="d-flex flex-wrap justify-content-center justify-content-sm-start">
+		<div class="row d-flex justify-content-center justify-content-sm-start">
 			<a href="/" class="col-auto d-flex">
                 <img src="/images/logo_white.svg" alt="Логотип белый" class="logo logo-white">
                 <img src="/images/logo_black.svg" alt="Логотип черный" class="logo logo-black">
 			</a>
 
-			<form class="col-12 col-sm-auto my-3 my-sm-0 mx-0 mx-sm-3 d-flex align-items-center">
+			<form class="col-12 col-sm-auto my-3 my-sm-0 d-flex align-items-center">
 				<input type="search" class="form-control form-control-dark " placeholder="Поиск..." aria-label="Search">
 			</form>
 		</div>
