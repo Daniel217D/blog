a2021ab6b5e314d070e0d9dfafab6f319252cf47
daniel21703 <daniel217032001@gmail.com>
2024-02-18 01:02:44 +0300

    Add api route for getting article

--- src/routes.php
+++ src/routes.php
@@ -11,6 +11,15 @@ $this->add_route( 'home', 'GET', '/', function () {
 	$this->render_page( 'Заметки', ( new Articles( $_GET['s'] ?? '' ) )->get_content_html() );
 } );
 
-$this->add_route( 'articles', 'GET', '/{slug}', function ( array $params ) {
+$this->add_route( 'article', 'GET', '/{slug}', function ( array $params ) {
 	$this->render_page( $params['slug'], Article::foundOrFail( $params['slug'] )->get_content_html() );
+} );
+
+$this->add_route( 'article_api', 'GET', '/api/articles/{slug}', function ( array $params ) {
+	$content = Article::foundOrFail( $params['slug'] )->get_content_html();
+
+	$this->send_json( array(
+		'success' => true,
+		'content' => $content,
+	) );
 } );
