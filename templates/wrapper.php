d0dab9dd4dbecad93bd9f1df07d059dd6722f412
daniel21703 <daniel217032001@gmail.com>
2024-02-18 19:58:03 +0300

    Add og meta for all pages

--- templates/wrapper.php
+++ templates/wrapper.php
@@ -13,6 +13,19 @@ use DDaniel\Blog\PageController;
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta name="description" content="<?php echo $pc->description ?>"/>
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+    <meta property="og:locale" content="ru_RU"/>
+    <meta property="og:type" content="<?php echo $pc->type ?>"/>
+    <meta property="og:title" content="<?php echo $pc->title ?>"/>
+    <meta property="og:description" content="<?php echo $pc->description ?>"/>
+    <meta property="og:url" content="<?php echo app()->current_url ?>"/>
+    <meta property="og:site_name" content="<?php echo app()->site_name ?>"/>
+    <meta property="og:image" content="<?php echo app()->site_url . '/images/logo_wide.png' ?>"/>
+    <meta property="og:image:secure_url" content="<?php echo app()->site_url . '/images/logo_wide.png' ?>"/>
+    <meta property="og:image:width" content="1024"/>
+    <meta property="og:image:height" content="512"/>
+
     <?php app()->assets->add_css('index') ?>
 
     <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon.png">
