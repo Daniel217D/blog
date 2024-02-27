09ea4327392e066f30466c540990b81ac6a17c51
daniel21703 <daniel217032001@gmail.com>
2024-02-28 00:36:21 +0300

    Update mdRender.js

--- assets/src/js/mdRender.js
+++ assets/src/js/mdRender.js
@@ -3,7 +3,8 @@ import markdownit from "markdown-it/index.mjs";
 export default function () {
     const md = markdownit()
     Array.from(document.querySelectorAll('.markdown')).forEach((el) => {
-        el.innerHTML = md.render(el.innerHTML).replaceAll('h1>', 'h2>');
+        const html =  md.render(el.innerHTML).replaceAll('h1>', 'h2>').replaceAll('&amp;', '&');
+        el.insertAdjacentHTML('afterbegin', html);
         el.style.display = '';
     })
 }
