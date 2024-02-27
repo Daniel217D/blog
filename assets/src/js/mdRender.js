a56731a516df899a15b238ff177dc01a20b20976
daniel21703 <daniel217032001@gmail.com>
2024-02-28 00:51:29 +0300

    Update markdown-it config

--- assets/src/js/mdRender.js
+++ assets/src/js/mdRender.js
@@ -1,7 +1,9 @@
 import markdownit from "markdown-it/index.mjs";
 
 export default function () {
-    const md = markdownit()
+    const md = markdownit({
+        breaks: true
+    })
     Array.from(document.querySelectorAll('.markdown')).forEach((el) => {
         el.innerHTML = md.render(el.innerHTML).replaceAll('h1>', 'h2>').replaceAll('&amp;', '&');
         el.style.display = '';
