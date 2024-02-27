7aea74355eca43799aceeebd31a5c36d1ba70301
daniel21703 <daniel217032001@gmail.com>
2024-02-28 00:38:44 +0300

    Fix mdRender.js

--- assets/src/js/mdRender.js
+++ assets/src/js/mdRender.js
@@ -3,8 +3,7 @@ import markdownit from "markdown-it/index.mjs";
 export default function () {
     const md = markdownit()
     Array.from(document.querySelectorAll('.markdown')).forEach((el) => {
-        const html =  md.render(el.innerHTML).replaceAll('h1>', 'h2>').replaceAll('&amp;', '&');
-        el.insertAdjacentHTML('afterbegin', html);
+        el.innerHTML = md.render(el.innerHTML).replaceAll('h1>', 'h2>').replaceAll('&amp;', '&');
         el.style.display = '';
     })
 }
