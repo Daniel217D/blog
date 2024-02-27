70fba81a58835f1309ed6c573be576d28ce0a8b9
daniel21703 <daniel217032001@gmail.com>
2024-02-28 00:35:52 +0300

    Update links at head

--- templates/wrapper.php
+++ templates/wrapper.php
@@ -13,25 +13,25 @@ use DDaniel\Blog\PageController;
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <meta name="description" content="<?php echo $pc->description ?>"/>
-    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
-    <meta property="og:locale" content="ru_RU"/>
-    <meta property="og:type" content="<?php echo $pc->type ?>"/>
-    <meta property="og:title" content="<?php echo $pc->title ?>"/>
-    <meta property="og:description" content="<?php echo $pc->description ?>"/>
-    <meta property="og:url" content="<?php echo app()->current_url ?>"/>
-    <meta property="og:site_name" content="<?php echo app()->site_name ?>"/>
-    <meta property="og:image" content="<?php echo app()->site_url . '/images/logo_wide.png' ?>"/>
-    <meta property="og:image:secure_url" content="<?php echo app()->site_url . '/images/logo_wide.png' ?>"/>
-    <meta property="og:image:width" content="1024"/>
-    <meta property="og:image:height" content="512"/>
+    <meta name="description" content="<?php echo $pc->description ?>">
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large">
+    <meta property="og:locale" content="ru_RU">
+    <meta property="og:type" content="<?php echo $pc->type ?>">
+    <meta property="og:title" content="<?php echo $pc->title ?>">
+    <meta property="og:description" content="<?php echo $pc->description ?>">
+    <meta property="og:url" content="<?php echo app()->current_url ?>">
+    <meta property="og:site_name" content="<?php echo app()->site_name ?>">
+    <meta property="og:image" content="<?php echo app()->site_url . '/images/logo_wide.png' ?>">
+    <meta property="og:image:secure_url" content="<?php echo app()->site_url . '/images/logo_wide.png' ?>">
+    <meta property="og:image:width" content="1024">
+    <meta property="og:image:height" content="512">
 
     <?php app()->assets->add_css('index') ?>
 
-    <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo app()->site_url . '/favicons/apple-touch-icon.png' ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo app()->site_url . '/favicons/favicon-32x32.png' ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo app()->site_url . '/favicons/favicon-16x16.png' ?>">
+    <link rel="mask-icon" href="<?php echo app()->site_url . '/favicons/safari-pinned-tab.svg' ?>" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
