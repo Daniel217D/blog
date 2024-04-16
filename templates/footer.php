41eaa81cdb43475086e604a4e5b3cab5d63229e5
daniel21703 <daniel217032001@gmail.com>
2024-04-16 22:07:33 +0300

    Fix light theme

--- templates/footer.php
+++ templates/footer.php
@@ -3,12 +3,13 @@
 declare(strict_types=1);
 
 ?>
-<footer class="p-sm-3 py-3 mt-2 bg-dark text-white border-top">
+<footer class="p-sm-3 py-3 mt-2 border-top">
     <div class="container">
         <div class="row d-flex flex-column flex-sm-row align-items-center">
             <div class="col-auto d-flex align-items-center my-1">
                 <a href="/" class="text-muted text-decoration-none">
                     <img src="/images/logo_white.svg" alt="Логотип белый" class="logo logo-white">
+                    <img src="/images/logo_black.svg" alt="Логотип белый" class="logo logo-black">
                 </a>
             </div>
 
--- templates/header.php
+++ templates/header.php
@@ -3,11 +3,12 @@
 declare(strict_types=1);
 
 ?>
-<header class="p-sm-3 pt-3 mb-2 bg-dark text-white border-bottom">
+<header class="p-sm-3 pt-3 mb-2 border-bottom">
     <div class="container">
         <div class="row d-flex justify-content-center justify-content-sm-start">
             <a href="/" class="col-auto d-flex">
                 <img src="/images/logo_white.svg" alt="Логотип белый" class="logo logo-white">
+                <img src="/images/logo_black.svg" alt="Логотип белый" class="logo logo-black">
             </a>
 
             <form class="col-12 col-sm-auto my-3 my-sm-0 d-flex align-items-center" action="/" method="GET">
