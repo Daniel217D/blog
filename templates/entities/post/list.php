f82e0f6693e29af92d3b5b3a54f61e5bdba01c96
daniel21703 <daniel217032001@gmail.com>
2024-05-06 00:47:33 +0300

    Update date format in posts list

--- templates/entities/post/list.php
+++ templates/entities/post/list.php
@@ -14,7 +14,7 @@ use DDaniel\Blog\Entities\Post;
         <a href="<?php echo app()->router->getUrlForEntityFrontend($entity) ?>" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-0"><?php echo $entity->getTitle() ?></h5>
-                <small><?php echo $entity->getCreatedTime()->format('j.m.Y') ?></small>
+                <small><?php echo $entity->getCreatedTime()->format('d.m.Y') ?></small>
             </div>
             <p class="mb-1"><?php echo $entity->getExcerpt() ?></p>
         </a>
