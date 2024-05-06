6647816c8508ab5fa13dee7a57ac10ccf1800ebb
daniel21703 <daniel217032001@gmail.com>
2024-05-06 22:34:50 +0300

    Add edit link to post page

--- templates/entities/post/item.php
+++ templates/entities/post/item.php
@@ -9,6 +9,13 @@ use DDaniel\Blog\Entities\Post;
 
 <h1><?php echo $entity->getTitle() ?></h1>
 
+<?php if( app()->isAuthorized ) : ?>
+    <a href="<?php echo app()->router->getRoutePath('adminEntityEdit', [
+            'entity' => 'post',
+            'id' => $entity->getId()
+    ]) ?>" target="_blank">(edit)</a>
+<?php endif; ?>
+
 <div class="markdown" style="display: none"><?php echo $entity->getContent() ?></div>
 
 
