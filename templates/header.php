12e7e12d7c5bab60786cdfeb37fd565a5f8d1bc9
daniel21703 <daniel217032001@gmail.com>
2024-05-06 22:49:44 +0300

    Update header on mobile

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 ?>
-<header class="p-sm-3 pt-3 mb-2 border-bottom">
+<header class="p-3 pt-3 mb-2 border-bottom">
     <div class="container">
         <div class="row d-flex justify-content-center justify-content-sm-start">
             <a href="/" class="col-auto d-flex">
